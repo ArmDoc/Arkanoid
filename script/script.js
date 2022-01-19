5280c02f788c965de01cd75956d1215112d56fc3
ArmDoc <zarman32@yandex.ru>
2022-01-19 17:46:35 +0400

    Refactoring

--- script/script.js
+++ script/script.js
@@ -5,14 +5,10 @@ canvas.width 	= 600;
 canvas.height 	= 600;
 var width 	= canvas.width;
 var height	= canvas.height;
-
 var boxHeight = 30;
-
 var min = 3;
 var max = 8;
-
 var timer 	= null;
-
 var boxes	= [];
 
 var paddle = {
@@ -27,7 +23,7 @@ var paddle = {
 }
 
 var ball	= {
-	r : 16,
+	r : 15,
 	x : width / 2,
 	y : height - 16 - 40 ,
 	dx : Math.random() < 0.5 ? -1 : 1,
@@ -81,7 +77,7 @@ function drawPaddle() {
 }
 
 function drawBox() {
-	for(var i = 0; i <= boxes.length-1; ++i) {
+	for(var i = 0; i < boxes.length; ++i) {
 		ctx.beginPath();
 		ctx.fillStyle = boxes[i].c
 		ctx.fillRect(boxes[i].x, boxes[i].y, boxes[i].w, boxes[i].h);
@@ -122,7 +118,7 @@ function calculations() {
 
 	if(ball.dy == 1 &&
 		ball.x + ball.dx + ball.r >= paddle.x + ball.r &&
-		ball.x + ball.dx - ball.r  <= paddle.x + paddle.w - ball.r) {
+		ball.x + ball.dx - ball.r <= paddle.x + paddle.w - ball.r) {
 		if(ball.y + ball.dy >= paddle.y - ball.r) {
 			ball.dy *= -1
 		}
@@ -131,11 +127,11 @@ function calculations() {
 	ball.x += ball.dx;
 	ball.y += ball.dy;
 
-	boxes.forEach(function (box, i ) {
-		if(ball.x +ball.r>= box.x && 
-			ball.x-ball.r <= box.x + box.w &&
-			ball.y+ball.r >= box.y &&
-			ball.y-ball.r <= box.y + box.h) {
+	boxes.forEach(function (box, i) {
+		if(ball.x + ball.r >= box.x && 
+			ball.x - ball.r <= box.x + box.w &&
+			ball.y + ball.r >= box.y &&
+			ball.y - ball.r <= box.y + box.h) {
 			boxes.splice(i, 1);
 			ball.dy *= -1;
 		}
@@ -167,7 +163,6 @@ document.addEventListener('keydown', function(e) {
 
 	if(e.keyCode === 82) {
 		if(boxes.length == 0 || ball.y + ball.r >= height){
-			
 			boxes	= [];
 
 			paddle = {
@@ -188,7 +183,6 @@ document.addEventListener('keydown', function(e) {
 				dx : Math.random() < 0.5 ? -1 : 1,
 				dy : -1,
 			}
-
 			createCubes();
 			draw();
 			calculations();
